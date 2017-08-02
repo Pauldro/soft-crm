f87483d4fe4aaf516f716c83c1cd9506fdd2affe
Paul Gomez <pauldro952@outlook.com>
2017-08-02 15:27:12 -0500

    Non stock item switch

--- site/templates/content/ajax/products-router.php
+++ site/templates/content/ajax/products-router.php
@@ -1,6 +1,5 @@
 <?php
-	if ($input->get->ordn) { $ordn = $input->get->text('ordn'); } else { $ordn = NULL; }
-	if ($input->get->qnbr) { $qnbr = $input->get->text('qnbr'); } else { $qnbr = NULL; }
+	$qnbr = $input->get->text('qnbr');
 	$title = '';
     $filteron = $input->urlSegment(3);
     switch ($filteron) {
@@ -8,7 +7,7 @@
             $custID = $input->get->text('custID');
             $shipID = '';
             $include = $config->paths->content.'products/ajax/load/product-results/product-results.php';
-			switch($input->urlSegment4) { 
+			switch($input->urlSegment4) {
 				case 'cart':
 					break;
 				case 'order':
@@ -18,18 +17,25 @@
 					break;
 			}
             break;
+        case 'non-stock':
+            switch($input->urlSegment4) {
+				default:
+					//$include = $config->paths->content.'products/non-stock/non-stock-item-form.php';
+					$modaltitle = 'Add Non-stock Item';
+					$modalbody = $config->paths->content.'products/non-stock/non-stock-item-form.php';
+					$include = $config->paths->content.'common/modals/include-ajax-modal-content.php';
+					break;
+            }
+            break;
 
     }
 
-	
+
 
 	if ($config->ajax) {
-		include($include);
+		include ($include);
 	} else {
-		
 		$modalbody = $include;
 		include $config->paths->content."common/include-blank-page.php";
 	}
 ?>
-
-
