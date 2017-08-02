6ce41fcc4753129da394d86cfbb3e1df00dded13
Paul Gomez <pauldro952@outlook.com>
2017-08-02 15:27:28 -0500

    Removed tasks and notes references

--- site/templates/content/common/initialize.php
+++ site/templates/content/common/initialize.php
@@ -1,29 +1,10 @@
 <?php
 
-	$tasktypes = array(
-		'email' => array(
-			'icon' => '<i class="material-icons md-18">&#xE0BE;</i>',
-			'value' => 'email',
-			'label' => 'Email'
-		),
-		'follow' => array(
-			'icon' => '<i class="glyphicon glyphicon-time"></i>',
-			'value' => 'follow',
-			'label' => 'Follow up'
-		),
-		'phone' => array(
-			'icon' => '<i class="glyphicon glyphicon-earphone"></i>',
-			'value' => 'phone',
-			'label' => 'call'
-		)
-	);
+
 
 	$sojson = file_get_contents($config->paths->site."so-config.json");
 	$soconfig = json_decode($sojson, true);
 
-	include $config->paths->assets."classes/crm/src/TaskClass.php";
-	include $config->paths->assets."classes/crm/src/NoteClass.php";
+	include $config->paths->assets."classes/crm/src/UserAction.class.php";
+	include $config->paths->assets."classes/crm/src/UserActionPanel.class.php";
 	include $config->paths->assets."classes/crm/src/ContactClass.php";
-	include $config->paths->assets."classes/crm/src/NotePanelClass.php";
-	include $config->paths->assets."classes/crm/src/TaskPanelClass.php";
-	//include $config->paths->assets."classes/TaskScheduler.php";
