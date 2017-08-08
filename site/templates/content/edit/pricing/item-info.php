46f6464ff64c3043c3444a3c85e2b2bb7bade7ab
Paul Gomez <pauldro952@outlook.com>
2017-08-08 17:18:01 -0500

    CHANGE item-info on editpricing
    Tabs ids and hrefs are now cleaned for Javascript

--- site/templates/content/edit/pricing/item-info.php
+++ site/templates/content/edit/pricing/item-info.php
@@ -15,20 +15,20 @@
             <h4><?= $item['itemid']; ?></h4> <h5><?= $item['name1']; ?></h5>
             <div class="product-info">
                 <ul class="nav nav-tabs nav_tabs hidden-print">
-                    <li class="active"><a href="#<?= $item['itemid']; ?>-desc-tab" data-toggle="tab" aria-expanded="true">Description</a></li>
-                    <li><a href="#<?= $item['itemid']; ?>-specs-tab" data-toggle="tab" aria-expanded="false">Specifications</a></li>
-                    <li><a href="#<?= $item['itemid']; ?>-pricing-tab" data-toggle="tab" aria-expanded="false">Pricings</a></li>
+                    <li class="active"><a href="#<?= cleanforjs($item['itemid']); ?>-desc-tab" data-toggle="tab" aria-expanded="true">Description</a></li>
+                    <li><a href="#<?= cleanforjs($item['itemid']); ?>-specs-tab" data-toggle="tab" aria-expanded="false">Specifications</a></li>
+                    <li><a href="#<?= cleanforjs($item['itemid']); ?>-pricing-tab" data-toggle="tab" aria-expanded="false">Pricings</a></li>
                     <?php if ($linedetail['kititemflag'] == 'Y') : ?>
-                        <li><a href="#<?= $item['itemid']; ?>-components-tab" data-toggle="tab" aria-expanded="false">Kit Components</a></li>
+                        <li><a href="#<?= cleanforjs($item['itemid']); ?>-components-tab" data-toggle="tab" aria-expanded="false">Kit Components</a></li>
                     <?php endif; ?>
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane fade active in" id="<?= $item['itemid']; ?>-desc-tab">
                         <br><p><?= $item['shortdesc']; ?></p>
                     </div>
-                    <div class="tab-pane fade" id="<?= $item['itemid']; ?>-specs-tab"><br><?php include $config->paths->content."products/product-results/product-features.php"; ?></div>
-                    <div class="tab-pane fade" id="<?= $item['itemid']; ?>-pricing-tab"><br><?php include $config->paths->content."products/product-results/price-structure.php"; ?></div>
-                    <div class="tab-pane fade" id="<?= $item['itemid']; ?>-components-tab"><br><?php include $config->paths->content."products/product-results/kit-components.php"; ?></div>
+                    <div class="tab-pane fade" id="<?= cleanforjs($item['itemid']); ?>-specs-tab"><br><?php include $config->paths->content."products/product-results/product-features.php"; ?></div>
+                    <div class="tab-pane fade" id="<?= cleanforjs($item['itemid']); ?>-pricing-tab"><br><?php include $config->paths->content."products/product-results/price-structure.php"; ?></div>
+                    <div class="tab-pane fade" id="<?= cleanforjs($item['itemid']); ?>-components-tab"><br><?php include $config->paths->content."products/product-results/kit-components.php"; ?></div>
                 </div>
             </div>
         <?php endif; ?>
