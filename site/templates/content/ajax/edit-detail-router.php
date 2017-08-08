0e8f58d961a4ebc7b81561254a35dc0930369294
Paul Gomez <pauldro952@outlook.com>
2017-08-08 17:15:05 -0500

    CHANGE edit-detail-router.php
    CHANGED $title to $page->title
    $body to $page->body

--- site/templates/content/ajax/edit-detail-router.php
+++ site/templates/content/ajax/edit-detail-router.php
@@ -1,15 +1,18 @@
 <?php
     $edittype = $input->urlSegment3; // CART || SALE
     $linenbr = $sanitizer->text($input->get->line);
+    if ($input->get->vendorID) {
+        $vendorID = $input->get->text('vendorID');
+    }
     switch ($edittype) {
         case 'cart':
             $linedetail = getcartline(session_id(), $linenbr, false);
-            $title = 'Edit Pricing for '. $linedetail['itemid'];
+            $page->title = 'Edit Pricing for '. $linedetail['itemid'];
             $custID = getcartcustomer(session_id(), false);
             $formaction = $config->pages->cart."redir/";
             $linedetail['can-edit'] = true;
             $ordn = '';
-			$body = $config->paths->content."edit/pricing/edit-pricing-form.php";
+			$page->body = $config->paths->content."edit/pricing/edit-pricing-form.php";
             break;
         case 'order':
             $ordn = $input->get->text('ordn');
@@ -18,30 +21,28 @@
             if (caneditorder(session_id(), $ordn, false) && $ordn == getlockedordn(session_id())) {
                 $linedetail['can-edit'] = true;
                 $formaction = $config->pages->orders."redir/";
-                $title = 'Edit Pricing for '. $linedetail['itemid'];
+                $page->title = 'Edit Pricing for '. $linedetail['itemid'];
             } else {
                 $linedetail['can-edit'] = false;
                 $formaction = '';
-                $title = 'Viewing Details for '. $linedetail['itemid'];
+                $page->title = 'Viewing Details for '. $linedetail['itemid'];
             }
-			$body = $config->paths->content."edit/pricing/edit-pricing-form.php";
+			$page->body = $config->paths->content."edit/pricing/edit-pricing-form.php";
             break;
 		case 'quote':
 			$qnbr = $input->get->text('qnbr');
 			$custID = getquotecustomer(session_id(), $qnbr, false);
 			$linedetail = get_quoteline(session_id(), $qnbr, $linenbr, false);
 			$formaction = $config->pages->quotes."redir/";
-            $title = 'Edit Pricing for '. $linedetail['itemid'];
+            $page->title = 'Edit Pricing for '. $linedetail['itemid'];
             $linedetail['can-edit'] = true;
-			$body = $config->paths->content."edit/pricing/quotes/edit-pricing-form.php";
+			$page->body = $config->paths->content."edit/pricing/quotes/edit-pricing-form.php";
     }
 	if ($config->ajax) {
-		$modalbody = $body;
-		$modaltitle = $title;
-		include $config->paths->content."common/modals/include-add-item-modal.php";
+        if ($config->modal) {
+            include $config->paths->content."common/modals/include-ajax-modal.php";
+        }
 	} else {
-		$page->title = $title;
-		$modalbody = $body;
 		include $config->paths->content."common/include-blank-page.php";
 	}
 
