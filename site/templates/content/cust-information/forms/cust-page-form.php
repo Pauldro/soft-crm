0cdbb6f9226459406919f083ff49ae10f6b0276b
Paul Gomez <pauldro952@outlook.com>
2017-08-10 09:31:50 -0500

    CHANGE cust-info cust index input
    changed .custid to .custID so it loads cust correctly

--- site/templates/content/cust-information/forms/cust-page-form.php
+++ site/templates/content/cust-information/forms/cust-page-form.php
@@ -5,7 +5,7 @@
     <input type="hidden" name="shiptocount" class="shiptocount" value="<?php echo $custjson['shiptocount']; ?>">
     <div class="form-group">
         <div class="input-group custom-search-form">
-            <input type="text" class="form-control input-sm not-round custid" name="custid" placeholder="Search custID" value="<?php echo $custID;  ?>">
+            <input type="text" class="form-control input-sm not-round custID" name="custID" placeholder="Search custID" value="<?php echo $custID;  ?>">
             <span class="input-group-btn">
             	<button type="submit" class="btn btn-sm btn-default not-round"> <span class="glyphicon glyphicon-search"></span> </button>
             </span>
