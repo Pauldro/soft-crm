4651dde610735d9e2b11a6bd0f53d446f34a3d67
Paul Gomez <pauldro952@outlook.com>
2017-09-15 16:05:36 -0500

    Saving files before refreshing line endings

--- wire/core/FileValidatorModule.php
+++ wire/core/FileValidatorModule.php
@@ -2,23 +2,23 @@
 
 /**
  * Base class for FileValidator modules
- * 
- * To create a FileValidator module: 
- * 
+ *
+ * To create a FileValidator module:
+ *
  * 1. Create a class that extends this and follow the naming convention: FileValidator[Something].
  * 2. Place in file: /site/modules/FileValidator[Something].module (or in module-specific directory).
  * 3. Copy the getModuleInfo() method out of this class and update as appropriate.
- * 4. Implement an isValidFile($filename) method, and you are done. 
- * 
+ * 4. Implement an isValidFile($filename) method, and you are done.
+ *
  * EXAMPLE: /site/modules/FileValidatorSVG.module
- * 
+ *
  * class FileValidatorHTML extends FileValidatorModule {
  *     public static function getModuleInfo() {
  *       return array(
  *         'title' => 'Validate HTML files',
- *         'version' => 1, 
- *         'autoload' => false, 
- *         'singular' => false, 
+ *         'version' => 1,
+ *         'autoload' => false,
+ *         'singular' => false,
  *         'validates' => array('html', 'htm'),
  *         'requires' => 'ProcessWire>=2.5.24',
  *       );
@@ -29,23 +29,23 @@
  *         return $valid;
  *     }
  * }
- * 
+ *
  * This file is licensed under the MIT license
  * https://processwire.com/about/license/mit/
- * 
+ *
  * Class FileValidatorModule
- * 
+ *
  */
 abstract class FileValidatorModule extends WireData implements Module {
-	
+
 	/**
 	 * Get module information
-	 * 
-	 * FileValidator modules should provide their own getModuleInfo() with the 
-	 * key part being the 'validates' property (see below). 
-	 * 
+	 *
+	 * FileValidator modules should provide their own getModuleInfo() with the
+	 * key part being the 'validates' property (see below).
+	 *
 	 * @return array
-	 * 
+	 *
 	 */
 	public static function getModuleInfo() {
 		return array(
@@ -60,7 +60,7 @@ abstract class FileValidatorModule extends WireData implements Module {
 			// Required of all FileValidator modules: specify extensions they validate as array.
 			// Extensions should be lowercase. To specify a regex, start and end with a "/" slash.
 			// If module wants to validate all file types, it should specify: "/.*/"
-				
+
 			'validates' => array(
 				'xyz',		// filename.xyz
 				'123',		// filename.123
@@ -68,7 +68,7 @@ abstract class FileValidatorModule extends WireData implements Module {
 			)
 		);
 	}
-	
+
 	/**
 	 * Page object associated with files, if applicable
 	 *
@@ -95,32 +95,32 @@ abstract class FileValidatorModule extends WireData implements Module {
 
 	/**
 	 * Is the given file valid? (this is the method mdoules should implement)
-	 * 
+	 *
 	 * This method should return:
 	 * 	- boolean TRUE if file is valid
 	 * 	- boolean FALSE if file is not valid
 	 * 	- integer 1 if file is valid as a result of sanitization performed by this method (if supported by module)
-	 * 	
-	 * If the file can be made valid by sanitization, this method may also choose to do that (perhaps if configured 
-	 * to do so) and return integer 1 after doing so. 
-	 * 
-	 * If method wants to explain why the file is not valid, it should call $this->error('reason why not valid'). 
-	 * 
+	 *
+	 * If the file can be made valid by sanitization, this method may also choose to do that (perhaps if configured
+	 * to do so) and return integer 1 after doing so.
+	 *
+	 * If method wants to explain why the file is not valid, it should call $this->error('reason why not valid').
+	 *
 	 * @param string $filename Full path and filename to the file
-	 * @return bool|int 
-	 * 
+	 * @return bool|int
+	 *
 	 */
 	abstract protected function isValidFile($filename);
 
 	/**
 	 * Is the given file valid?
-	 * 
+	 *
 	 * FileValidator modules should not implement this method, as it only serves as a front-end to isValid()
-	 * for logging purposes. 
-	 * 
+	 * for logging purposes.
+	 *
 	 * @param $filename
 	 * @return bool|int Returns TRUE if valid, FALSE if not, or integer 1 if valid as a result of sanitization.
-	 * 
+	 *
 	 */
 	final public function isValid($filename) {
 		$valid = $this->isValidFile($filename);
@@ -134,8 +134,8 @@ abstract class FileValidatorModule extends WireData implements Module {
 			}
 		} else {
 			$message .= $this->_('INVALID') . ": $filename";
-			$errors = $this->errors('array'); 
-			if(count($errors)) $message .= " - " . implode(', ', $errors); 
+			$errors = $this->errors('array');
+			if(count($errors)) $message .= " - " . implode(', ', $errors);
 		}
 		$this->log($message);
 		return $valid;
@@ -143,11 +143,11 @@ abstract class FileValidatorModule extends WireData implements Module {
 
 	/**
 	 * Get the Page associated with any isValid() calls
-	 * 
+	 *
 	 * If not applicable, it will be a NullPage()
-	 * 
+	 *
 	 * @return NullPage|Page
-	 * 
+	 *
 	 */
 	public function getPage() {
 		$page = $this->_page ? $this->_page : null;
@@ -171,25 +171,25 @@ abstract class FileValidatorModule extends WireData implements Module {
 
 	/**
 	 * Get the Pagefile or Pageimage object associated with any isValid() calls
-	 * 
+	 *
 	 * If not applicable, it will be null.
-	 * 
+	 *
 	 * @return Pagefile|null
-	 * 
+	 *
 	 */
 	public function getPagefile() {
 		$pagefile = $this->_pagefile ? $this->_pagefile : null;
 		return $pagefile;
 	}
-	
+
 	public function setPage(Page $page) {
 		$this->_page = $page;
 	}
-	
+
 	public function setField(Field $field) {
 		$this->_field = $field;
 	}
-	
+
 	public function setPagefile(Pagefile $pagefile) {
 		$this->_pagefile = $pagefile;
 	}
@@ -211,5 +211,5 @@ abstract class FileValidatorModule extends WireData implements Module {
 		if(empty($options['name'])) $options['name'] = 'file-validator';
 		return parent::___log($str, $options);
 	}
-	
+
 }
