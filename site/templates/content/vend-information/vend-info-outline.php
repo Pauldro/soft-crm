492745bff6b52225d4145270f7182328f9ac6b85
bbullemer <barbara.bullemer@gmail.com>
2017-10-10 11:55:57 -0500

    #45 VI Main Screen - vendor info outline
    added logic to get search bar for vendor ID
    created vend-page-form

--- site/templates/content/vend-information/vend-info-outline.php
+++ site/templates/content/vend-information/vend-info-outline.php
@@ -9,14 +9,19 @@ $vendjson = json_decode(file_get_contents($config->jsonfilepath."vitest-vivendor
         <?php $topcolumns = array_keys($vendjson['columns']['top']); ?>
         <?php foreach ($topcolumns as $column ) : ?>
             <?php if ($vendjson['columns']['top'][$column]['heading'] == '' && $vendjson['data']['top'][$column] == '') : ?>
-                <!-- TODO need search bar for vendor ID -->
             <?php else : ?>
                 <tr>
                     <td class="<?= $config->textjustify[$vendjson['columns']['left'][$column]['headingjustify']]; ?>">
                         <?php echo $vendjson['columns']['top'][$column]['heading']; ?>
                     </td>
                     <td>
-                        <?php echo $vendjson['data']['top'][$column]; ?>
+                        <?php
+                            if ($column == 'vendorid') {
+                                include $config->paths->content."vend-information/forms/vend-page-form.php";
+                            } else {
+                                echo $vendjson['data']['top'][$column];
+                            }
+                        ?>
                     </td>
                 </tr>
             <?php endif; ?>
