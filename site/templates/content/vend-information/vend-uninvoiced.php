3464d3e6abf871bf332c67de92af9c06b6f12b3b
bbullemer <barbara.bullemer@gmail.com>
2017-11-17 13:32:50 -0600

    VI Uninvoiced Screen
    code was made more efficient

--- site/templates/content/vend-information/vend-uninvoiced.php
+++ site/templates/content/vend-information/vend-uninvoiced.php
@@ -1,6 +1,6 @@
 <?php
-	$uninvoicedfile = $config->jsonfilepath.session_id()."-viuninvoiced.json";
-	// $uninvoicedfile = $config->jsonfilepath."viuni-viuninvoiced.json";
+	 $uninvoicedfile = $config->jsonfilepath.session_id()."-viuninvoiced.json";
+	//$uninvoicedfile = $config->jsonfilepath."viuni-viuninvoiced.json";
 	
 	if ($config->ajax) {
 		echo $page->bootstrap->openandclose('p', '', $page->bootstrap->makeprintlink($config->filename, 'View Printable Version'));
@@ -17,76 +17,78 @@
 			$headercolumns = array_keys($uninvoicedjson['columns']['header']);
 		    $detailcolumns = array_keys($uninvoicedjson['columns']['details']);
 			
-			$headercount = count($headercolumns);
-			$detailcount = count($detailcolumns);
+			$columncount = $detailcolumns > $headercolumns ? sizeof($detailcolumns) : sizeof($headercolumns);
 			
-			if ($headercount > $detailcount) {
-				$maxcolumns = $headercount;
-			} else {
-				$maxcolumns = $detailcount;
-			}
-			
-				if (sizeof($uninvoicedjson['data']) > 0) {
+			if (sizeof($uninvoicedjson['data']) > 0) {
+				$tb = new Table('class=table table-striped table-bordered table-condensed table-excel|id=uninvoiced');
+				$tb->tablesection('thead');
+					$tb->tr();
+					foreach ($headercolumns as $column) {
+						$class = $config->textjustify[$uninvoicedjson['columns']['header'][$column]['headingjustify']];
+						$tb->th("class=$class", $uninvoicedjson['columns']['header'][$column]['heading']);
+					}
 					
-					$tb = new Table('class=table table-striped table-bordered table-condensed table-excel|id=uninvoiced');
-					$tb->tablesection('thead');
-						$tb->tr();
+					if (sizeof($headercolumns) < $columncount) {
+						for ($i = 0; $i < ($columncount - sizeof($headercolumns)); $i++) {
+							$tb->th('');
+						}
+					}
+					
+					$tb->tr();
+					foreach ($detailcolumns as $column) {
+						$class = $config->textjustify[$uninvoicedjson['columns']['details'][$column]['headingjustify']];
+						$tb->th("class=$class", $uninvoicedjson['columns']['details'][$column]['heading']);
+					}
+				$tb->closetablesection('thead');
+				$tb->tablesection('tbody');
+					 foreach ($uninvoicedjson['data']['purchaseorders'] as $order) {
+						$tb->tr('');
+					
 						foreach ($headercolumns as $column) {
-							$class = $config->textjustify[$uninvoicedjson['columns']['header'][$column]['headingjustify']];
-							$tb->th("class=$class", $uninvoicedjson['columns']['header'][$column]['heading']);
+							$class = $config->textjustify[$uninvoicedjson['columns']['header'][$column]['datajustify']];
+							$tb->td("class=$class", $order[$column]);
 						}
-						$tb->tr();
+						
+						if (sizeof($headercolumns) < $columncount) {
+							for ($i = 0; $i < ($columncount - sizeof($headercolumns)); $i++) {
+								$tb->td('');
+							}
+						}
+						
+						foreach ($order['details'] as $detail) {
+							$tb->tr('');
+							
+							foreach ($detailcolumns as $column) {
+								$class = $config->textjustify[$uninvoicedjson['columns']['details'][$column]['datajustify']];
+ 								$tb->td("class=$class", $detail[$column]);
+							}
+						}
+						
+						// Purchase Order Total
+						$total = $order['totals'];
+						$tb->tr('');
 						foreach ($detailcolumns as $column) {
-							$class = $config->textjustify[$uninvoicedjson['columns']['details'][$column]['headingjustify']];
-							$tb->th("class=$class", $uninvoicedjson['columns']['details'][$column]['heading']);
+							$class = $config->textjustify[$uninvoicedjson['columns']['details'][$column]['datajustify']];
+							$tb->td("class=$class", $total[$column]);
 						}
-					$tb->closetablesection('thead');
-					$tb->tablesection('tbody');
-						$maxrows = count($uninvoicedjson['data']['purchaseorders']);
-						for ($row = 1; $row < $maxrows; $row++) {
-							$tb->tr('');
-								foreach ($uninvoicedjson['data']['purchaseorders'][$row] as $order) {
-									if (!is_array($order)) {
-										$key = array_search($order, ($uninvoicedjson['data']['purchaseorders'][$row]));
-										$class = $config->textjustify[$uninvoicedjson['columns']['header'][$key]['datajustify']];
-										$tb->td("class=$class", $order);
-									} else {
-										$tb->td('');
-										break;
-									}
-								}
-							$detailmaxrows = count($uninvoicedjson['data']['purchaseorders'][$row]['details']);
-							for ($i = 1; $i < $detailmaxrows + 1; $i++) {
-								$tb->tr('');
-								foreach ($uninvoicedjson['data']['purchaseorders'][$row]['details'][$i] as $detail) {
-									$key = array_search($detail, ($uninvoicedjson['data']['purchaseorders'][$row]['details'][$i]));
-									$class = $config->textjustify[$uninvoicedjson['columns']['details'][$key]['datajustify']];
-									$tb->td("class=$class", $detail);
-								}
-							}
-							$tb->tr('');
-								foreach ($uninvoicedjson['data']['purchaseorders'][$row]['totals'] as $total) {
-									$key = array_search($total, ($uninvoicedjson['data']['purchaseorders'][$row]['totals']));
-									$class = $config->textjustify[$uninvoicedjson['columns']['details'][$key]['datajustify']];
-									$tb->td("class=$class", $total);
-								}
-							$tb->tr('class=last-section-row');
-								for ($x = 1; $x < $maxcolumns + 1; $x++) {
-									$tb->td('');
-								}
-							}
-						$tb->tr('class=bg-primary');
-							foreach ($uninvoicedjson['data']['vendortotals'] as $total) {
-								$key = array_search($total, ($uninvoicedjson['data']['vendortotals']));
-								$class = $config->textjustify[$uninvoicedjson['columns']['details'][$key]['datajustify']];
-								$tb->td("class=$class", $total);
-							}
-					$tb->closetablesection('tbody');
-					echo $tb->close();
-				}
+						
+						$tb->tr('class=last-section-row');
+						$tb->td('colspan='.sizeof($uninvoicedjson['columns']['details']));
+					}
+					
+					$vendortotals = $uninvoicedjson['data']['vendortotals'];
+					$tb->tr('class=bg-primary');
+					
+					foreach ($detailcolumns as $column) {
+						$class = $config->textjustify[$uninvoicedjson['columns']['details'][$column]['datajustify']];
+						$tb->td("class=$class", $vendortotals[$column]);
+					}
+					
+				$tb->closetablesection('tbody');
+				echo $tb->close();
 			}
-			
-		} else {
-			echo $page->bootstrap->createalert('warning', 'Information not available.');
 		}
+	} else {
+		echo $page->bootstrap->createalert('warning', 'Information not available.');
+	}
 ?>
