1a70dbdda9ded6bb458aca8f10da01939e2a5939
bbullemer <barbara.bullemer@gmail.com>
2017-11-17 09:25:51 -0600

    VI Notes Screen
    changed from table to display to list groups in a panel

--- site/templates/content/vend-information/vend-notes.php
+++ site/templates/content/vend-information/vend-notes.php
@@ -18,43 +18,37 @@
 			$purchasecolumns = array_keys($notesjson['columns']['purchase order notes']);
 			
 			if (sizeof($notesjson['data']) > 0) {
-				echo '<div class="row">';
-                    echo '<div class="col-sm-6">';
-    					$tb = new Table('class=table table-striped table-bordered table-condensed table-excel');
-                        $tb->tablesection('thead');
-    					foreach ($internalcolumns as $column) {
-    						$tb->tr();
-    						$tb->th('class='.$config->textjustify[$notesjson['columns']['internal notes'][$column]['headingjustify']], $notesjson['columns']['internal notes'][$column]['heading']);
-                            $tb->closetablesection('thead');
-                            $tb->tablesection('tbody');
-                            $internalcount = count($notesjson['data']['internal notes']);
-                            for ($i = 1; $i < $internalcount + 1; $i++) {
-                                $tb->tr();
-                                $tb->td('class='.$config->textjustify[$notesjson['columns']['internal notes'][$column]['datajustify']], $notesjson['data']['internal notes']["$i"]['internal note']);
-                            }
-    					}
-                        $tb->closetablesection('tbody');
-    					echo $tb->close();
-    				echo '</div>';
-
-    				echo '<div class="col-sm-6">';
-                        $tb = new Table('class=table table-striped table-bordered table-condensed table-excel');
-                        $tb->tablesection('thead');
-                        foreach ($purchasecolumns as $column) {
-                            $tb->tr();
-                            $tb->th('class='.$config->textjustify[$notesjson['columns']['purchase order notes'][$column]['headingjustify']], $notesjson['columns']['purchase order notes'][$column]['heading']);
-                            $tb->closetablesection('thead');
-                            $tb->tablesection('tbody');
-                            $purchasecount = count($notesjson['data']['purchase order notes']);
-                            for ($i = 1; $i < $purchasecount + 1; $i++) {
-                                $tb->tr();
-                                $tb->td('class='.$config->textjustify[$notesjson['columns']['purchase order notes'][$column]['datajustify']], $notesjson['data']['purchase order notes']["$i"]['purchase order note']);
-                            }
-                        }
-                        $tb->closetablesection('tbody');
-    					echo $tb->close();
-    				echo '</div>';
-                echo '</div>';    
+				echo '<div class="panel panel-primary">';
+					foreach ($internalcolumns as $column) {
+						$internalnotestitle = $notesjson['columns']['internal notes'][$column]['heading'];
+						echo '<div class="panel-heading">';
+							echo "<h4>$internalnotestitle</h4>";
+						echo '</div>';
+					}
+					echo '<div class="panel-body list-group">';
+						$internalcount = count($notesjson['data']['internal notes']);
+						for ($i = 1; $i < $internalcount + 1; $i++) {
+							$internalnotesdata = $notesjson['data']['internal notes'][$i]['internal note'];
+							echo "<p class='list-group-item'>$internalnotesdata</p>";
+						}
+					echo '</div>';
+				echo '</div>';
+						
+				echo '<div class="panel panel-primary">';
+					foreach ($purchasecolumns as $column) {
+						$purchasenotestitle = $notesjson['columns']['purchase order notes'][$column]['heading'];
+						echo '<div class="panel-heading">';
+							echo "<h4>$purchasenotestitle</h4>";
+						echo '</div>';
+					}
+					echo '<div class="panel-body list-group">';
+						$purchasecount = count($notesjson['data']['purchase order notes']);
+						for ($i = 1; $i < $purchasecount + 1; $i++) {
+							$purchasenotesdata = $notesjson['data']['purchase order notes'][$i]['purchase order note'];
+							echo "<p class='list-group-item'>$purchasenotesdata</p>";
+						}
+					echo '</div>';
+				echo '</div>';
 			} else {
 				echo $page->bootstrap->createalert('warning', 'Information Not Available'); 
 			} // END if (sizeof($notesjson['data']) > 0)
