436d65f3fe7cc95fb26e724fdffb205383419bf2
bbullemer <barbara.bullemer@gmail.com>
2017-11-28 13:09:49 -0600

    #109 Vender Costing
    removed shipfromID from search form - not needed

--- site/templates/content/vend-information/forms/item-search-form.php
+++ site/templates/content/vend-information/forms/item-search-form.php
@@ -2,7 +2,6 @@
 	<input type="text" class="form-control vi-item-search" name="q" autocomplete="off">
 	<input type="hidden" name="action" value="<?= $action; ?>">
 	<input type="hidden" name="vendorID" class="vendorid" value="<?php echo $vendorID; ?>">
-	<input type="hidden" name="shipfromID" class="shipfromid" value="<?php echo $shipfromID; ?>">
 	<div>
 		<?php include $config->paths->content."vend-information/item-search-results.php"; ?>
 	</div>
