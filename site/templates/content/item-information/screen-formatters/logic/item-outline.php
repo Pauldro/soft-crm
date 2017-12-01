c7867cb7eb1121db746ea3564db02901be2dce0a
bbullemer <barbara.bullemer@gmail.com>
2017-12-01 12:23:00 -0600

    #106
    Changes to get formatter working
    having issues getting example to load
    having issues getting item info page to load formatted table

--- site/templates/content/item-information/screen-formatters/logic/item-outline.php
+++ site/templates/content/item-information/screen-formatters/logic/item-outline.php
@@ -6,29 +6,29 @@
 		$formatterjson = json_decode(file_get_contents($default), true);
 	}
 
-	$detailcolumns = array_keys($formatterjson['detail']['columns']);
+	$detailcolumns = array_keys($formatterjson['header']['columns']);
     
 	$fieldsjson = json_decode(file_get_contents($config->companyfiles."json/iihfmattbl.json"), true);
 
 	$table = array(
 		'maxcolumns' => $formatterjson['cols'],
-		'detail' => array('maxrows' => $formatterjson['detail']['rows'], 'rows' => array()),
+		'header' => array('maxrows' => $formatterjson['header']['rows'], 'rows' => array()),
 	);
 
-	for ($i = 1; $i < $formatterjson['detail']['rows'] + 1; $i++) {
-		$table['detail']['rows'][$i] = array('columns' => array());
+	for ($i = 1; $i < $formatterjson['header']['rows'] + 1; $i++) {
+		$table['header']['rows'][$i] = array('columns' => array());
 		foreach($detailcolumns as $column) {
-			if ($formatterjson['detail']['columns'][$column]['line'] == $i) {
+			if ($formatterjson['header']['columns'][$column]['line'] == $i) {
 				$col = array(
 					'id' => $column, 
-					'label' => $formatterjson['detail']['columns'][$column]['label'], 
-					'column' => $formatterjson['detail']['columns'][$column]['column'], 
-					'col-length' => $formatterjson['detail']['columns'][$column]['col-length'], 
-					'before-decimal' => $formatterjson['detail']['columns'][$column]['before-decimal'],
-					'after-decimal' => $formatterjson['detail']['columns'][$column]['after-decimal'], 
-					'date-format' => $formatterjson['detail']['columns'][$column]['date-format']
+					'label' => $formatterjson['header']['columns'][$column]['label'], 
+					'column' => $formatterjson['header']['columns'][$column]['column'], 
+					'col-length' => $formatterjson['header']['columns'][$column]['col-length'], 
+					'before-decimal' => $formatterjson['header']['columns'][$column]['before-decimal'],
+					'after-decimal' => $formatterjson['header']['columns'][$column]['after-decimal'], 
+					'date-format' => $formatterjson['header']['columns'][$column]['date-format']
 				 );
-				$table['detail']['rows'][$i]['columns'][$formatterjson['detail']['columns'][$column]['column']] = $col;
+				$table['header']['rows'][$i]['columns'][$formatterjson['header']['columns'][$column]['column']] = $col;
 			}
 		}
 	}
--- site/templates/content/item-information/tables/item-outline-formatted.php
+++ site/templates/content/item-information/tables/item-outline-formatted.php
@@ -1,11 +1,11 @@
 <?php
 	$tb = new Table('class=table table-striped table-bordered table-condensed table-excel|id=');
 	$tb->tablesection('thead');
-		for ($x = 1; $x < $table['detail']['maxrows'] + 1; $x++) {
+		for ($x = 1; $x < $table['header']['maxrows'] + 1; $x++) {
 			$tb->tr();
 			for ($i = 1; $i < $table['maxcolumns'] + 1; $i++) {
-				if (isset($table['detail']['rows'][$x]['columns'][$i])) {
-					$column = $table['detail']['rows'][$x]['columns'][$i];
+				if (isset($table['header']['rows'][$x]['columns'][$i])) {
+					$column = $table['header']['rows'][$x]['columns'][$i];
 					$class = $config->textjustify[$fieldsjson['data']['header'][$column['id']]['headingjustify']];
 					$colspan = $column['col-length'];
 					$tb->th('colspan='.$colspan.'|class='.$class, $column['label']);
@@ -19,11 +19,11 @@
 	$tb->tablesection('tbody');
 		$tb->tr();
 		for ($i = 1; $i < $table['maxcolumns'] + 1; $i++) {
-			if (isset($table['detail']['rows'][$x]['columns'][$i])) {
-				$column = $table['detail']['rows'][$x]['columns'][$i];
-				$class = $config->textjustify[$fieldsjson['data']['detail'][$column['id']]['datajustify']];
+			if (isset($table['header']['rows'][$x]['columns'][$i])) {
+				$column = $table['header']['rows'][$x]['columns'][$i];
+				$class = $config->textjustify[$fieldsjson['data']['header'][$column['id']]['datajustify']];
 				$colspan = $column['col-length'];
-				$celldata = Table::generatejsoncelldata($fieldsjson['data']['detail'][$column['id']]['type'], $itemjson, $column);
+				$celldata = Table::generatejsoncelldata($fieldsjson['data']['header'][$column['id']]['type'], $itemjson, $column);
 				$tb->td('colspan='.$colspan.'|class='.$class, $celldata);
 				if ($colspan > 1) { $i = $i + ($colspan - 1); }
 			} else {
