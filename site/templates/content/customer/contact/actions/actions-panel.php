50fecab8add83398dbe01b1da3f4afe9925f652e
Paul Gomez <pauldro952@outlook.com>
2017-12-08 10:16:51 -0600

    UserActions Panel updates
    UPDATED site\templates\content\customer\contact\actions\actions-panel.php
    		site\templates\content\customer\cust-page\actions\actions-panel.php
    		site\templates\content\dashboard\actions\actions-panel.php
     - REMOVED LOGIC TO CHECK IF CUSTOMER HAS CUSTOM, SINCE NO CUSTOM YET, REMOVE CHECK.
    
    UPDATED site\templates\content\customer\contact\contact-page.php
     - ACTIONS PANEL IS NOW FOUND IN $config->paths->content."customer/contact/actions/actions-panel.php

--- site/templates/content/customer/contact/actions/actions-panel.php
+++ site/templates/content/customer/contact/actions/actions-panel.php
@@ -4,8 +4,4 @@
     $actionpanel->set_querylinks();
     $actionpanel->count_actions();
     
-    if (file_exists($config->paths->content."actions/$config->cptechcustomer-actions-panel.php")) {
-        include $config->paths->content."actions/$config->cptechcustomer-actions-panel.php";
-    } else {
-        include $config->paths->content."actions/actions-panel.php";
-    }
+    include $config->paths->content."actions/actions-panel.php";
--- site/templates/content/dashboard/actions/actions-panel.php
+++ site/templates/content/dashboard/actions/actions-panel.php
@@ -2,8 +2,4 @@
     $actionpanel = $page->useractionpanelfactory->create_actionpanel('user', session_id(), $config->ajax, $config->modal, $input->get->text('action-status'));
     $actionpanel->count_actions();
     
-    if (file_exists($config->paths->content."actions/$config->cptechcustomer-actions-panel.php")) {
-        include $config->paths->content."actions/$config->cptechcustomer-actions-panel.php";
-    } else {
-        include $config->paths->content."actions/actions-panel.php";
-    }
+    include $config->paths->content."actions/actions-panel.php";
