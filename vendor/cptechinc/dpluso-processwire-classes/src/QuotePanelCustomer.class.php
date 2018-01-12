94b97468a2ac5dfdefb222aa6395f6c59cfa7d16
bbullemer <barbara.bullemer@gmail.com>
2018-01-12 09:26:56 -0600

    #124 Cust Quote Sorting
    - added if statements to check which column was chosen for sorting

--- vendor/cptechinc/dpluso-processwire-classes/src/QuotePanelCustomer.class.php
+++ vendor/cptechinc/dpluso-processwire-classes/src/QuotePanelCustomer.class.php
@@ -25,10 +25,10 @@
             if ($this->tablesorter->orderby) {
                 if ($this->tablesorter->orderby == 'quotdate') {
                     $quotes = get_customerquotesquotedate($this->sessionID, $this->custID, Processwire\wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $useclass, $debug);
-                // } elseif ($this->tablesorter->orderby == 'revdate') {
-                //    $quotes = get_customerquotesrevdate($this->sessionID, $this->custID, Processwire\wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $useclass, $debug);
-                // } elseif ($this->tablesorter->orderby == 'expdate') {
-                //    $quotes = get_customerquotesexpdate($this->sessionID, $this->custID, Processwire\wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $useclass, $debug);
+                } elseif ($this->tablesorter->orderby == 'revdate') {
+                    $quotes = get_customerquotesrevdate($this->sessionID, $this->custID, Processwire\wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $useclass, $debug);
+                } elseif ($this->tablesorter->orderby == 'expdate') {
+                    $quotes = get_customerquotesexpdate($this->sessionID, $this->custID, Processwire\wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $useclass, $debug);
                 } else {
                     $quotes = get_customerquotesorderby($this->sessionID, $this->custID, Processwire\wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $this->tablesorter->orderby, $useclass, $debug);
                 }
