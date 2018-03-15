06bc68b40cbb06635aa9983b9211fe1b20ed19aa
bbullemer <barbara.bullemer@gmail.com>
2018-03-15 11:27:25 -0500

    Edit Customer Contact Info
    - changed editing page to one table instead of two

--- site/templates/content/customer/contact/contact-card.php
+++ site/templates/content/customer/contact/contact-card.php
@@ -8,92 +8,91 @@
     <div class="panel-heading not-round">
         <h3 class="panel-title">Details for <?php echo $contact->contact; ?></h3>
      </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <table class="table table-striped table-user-information">
-                <tbody>
-                    <tr>
-                        <td><strong>Address:</strong></td>
-                        <td>
-                            <strong>
-                                <?= $contact->addr1; ?><br>
-                                <?= (strlen($contact->addr2) > 0) ? $contact->addr2.'<br>' : ''; ?>
-                                <?= $contact->city . ', ' . $contact->state . ' ' . $contact->zip; ?>
-                            </strong>
-                        </td>
+        <table class="table table-striped table-user-information">
+            <tbody>
+                <div class="row">
+                    <div class="col-sm-6">
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        
+                    </div>
+                </div>
+                <tr>
+                    <td>Customer:</td>
+                    <td>
+                        <a href="<?= $contact->generate_customerurl(); ?>" target="_blank">
+                            <?= $contact->custid. ' - '. $contact->name ?> <i class="glyphicon glyphicon-share" aria-hidden="true"></i>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td><strong>Address:</strong></td>
+                    <td>
+                        <strong>
+                            <?= $contact->addr1; ?><br>
+                            <?= (strlen($contact->addr2) > 0) ? $contact->addr2.'<br>' : ''; ?>
+                            <?= $contact->city . ', ' . $contact->state . ' ' . $contact->zip; ?>
+                        </strong>
+                    </td>
+                </tr>
+                <?php if ($contact->has_shipto()) : ?>
+                    <tr> 
+                        <td>Shipto ID:</td> 
+                        <td><a href="<?= $contact->generate_shiptourl(); ?>" target="_blank"><?= $contact->shiptoid; ?> <i class="glyphicon glyphicon-share" aria-hidden="true"></i></a></td>
                     </tr>
-                    <tr>
-                        <td>Customer:</td>
-                        <td>
-                            <a href="<?= $contact->generate_customerurl(); ?>" target="_blank">
-                                <?= $contact->custid. ' - '. $contact->name ?> <i class="glyphicon glyphicon-share" aria-hidden="true"></i>
-                            </a>
-                        </td>
-                    </tr>
-                    <?php if ($contact->has_shipto()) : ?>
-                        <tr> 
-                            <td>Shipto ID:</td> 
-                            <td><a href="<?= $contact->generate_shiptourl(); ?>" target="_blank"><?= $contact->shiptoid; ?> <i class="glyphicon glyphicon-share" aria-hidden="true"></i></a></td>
-                        </tr>
-                    <?php endif; ?>
-                    <tr> <td>Title:</td><td><?= $contact->title; ?></td> </tr>
-                    <tr> <td>Email:</td> <td><a href="mailto:<?php echo $contact->email; ?>"><?php echo $contact->email; ?></a></td></tr>
-                    <tr>
-                        <td>Office Phone:</td>
-                        <td>
-                            <a href="tel:<?= $contact->phone; ?>"><?= $page->stringerbell->format_phone($contact->phone); ?></a><b> &nbsp;
-                            <?php if ($contact->has_extension()) { echo 'Ext. ' . $contact->extension;} ?></b>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Cell Phone:</td> <td><a href="tel:<?= $contact->cellphone; ?>"> <?= $page->stringerbell->format_phone($contact->cellphone); ?></a></td>
-                    </tr>
-                    <tr> <td>Fax:</td> <td><?= $contact->faxnbr; ?></td> </tr> 
-                </tbody>
-            </table>   
-        </div>
-        <div class="col-sm-6">
-            <table class="table table-striped table-user-information">
-                <tbody>
-                    <tr class="<?= $contact->has_shipto() ? 'hidden' : ''; ?>">
-    					<td class="control-label">AR Contact</td>
-    					<td>
-    						<?= $page->bootstrap->select('class=form-control input-sm|name=arcontact', array_flip($config->yesnoarray), 'N'); ?>
-    					</td>
-    				</tr>
-    				<tr class="<?= $contact->has_shipto() ? 'hidden' : ''; ?>">
-    					<td class="control-label">Dunning Contact</td>
-    					<td>
-    						<?= $page->bootstrap->select('class=form-control input-sm|name=dunningcontact', array_flip($config->yesnoarray), 'N'); ?>
-    					</td>
-    				</tr>
-    				<tr class="<?= $contact->has_shipto() ? 'hidden' : ''; ?>">
-    					<td class="control-label">AR Contact</td>
-    					<td>
-    						<?= array_flip($config->yesnoarray)[$contact->arcontact]; ?>
-    					</td>
-    				</tr>
-    				<tr>
-    					<td class="control-label">Buying Contact</td>
-    					<td>
-    						<?= $config->buyertypes[$contact->buyingcontact]; ?>
-    					</td>
-    				</tr>
-    				<tr>
-    					<?php if ($config->cptechcustomer == 'stat') : ?>
-    						<td class="control-label">End User</td>
-    					<?php else : ?>
-    						<td class="control-label">Certificate Contact</td>
-    					<?php endif; ?>
-    					
-    					<td>
-    						<?= array_flip($config->yesnoarray)[$contact->certcontact]; ?>
-    					</td>
-    				</tr>
-                </tbody>
-            </table> 
-        </div>
-    </div>
+                <?php endif; ?>
+                <tr> <td>Name:</td> <td><?= $contact->contact; ?></td> </tr>
+                <tr> <td>Title:</td><td><?= $contact->title; ?></td> </tr>
+                <tr> <td>Email:</td> <td><a href="mailto:<?php echo $contact->email; ?>"><?php echo $contact->email; ?></a></td></tr>
+                <tr>
+                    <td>Office Phone:</td>
+                    <td>
+                        <a href="tel:<?= $contact->phone; ?>"><?= $page->stringerbell->format_phone($contact->phone); ?></a><b> &nbsp;
+                        <?php if ($contact->has_extension()) { echo 'Ext. ' . $contact->extension;} ?></b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Cell Phone:</td> <td><a href="tel:<?= $contact->cellphone; ?>"> <?= $page->stringerbell->format_phone($contact->cellphone); ?></a></td>
+                </tr>
+                <tr> <td>Fax:</td> <td><a href="tel:<?= $contact->cellphone; ?>"> <?= $page->stringerbell->format_phone($contact->faxnbr); ?></a></td> </tr> 
+                <tr class="<?= $contact->has_shipto() ? 'hidden' : ''; ?>">
+    				<td class="control-label">AR Contact</td>
+    				<td>
+    					<?= array_flip($config->yesnoarray)[$contact->arcontact]; ?>
+    				</td>
+    			</tr>
+                <tr class="<?= $contact->has_shipto() ? 'hidden' : ''; ?>">
+    				<td class="control-label">Dunning Contact</td>
+    				<td>
+    					<?= array_flip($config->yesnoarray)[$contact->dunningcontact]; ?>
+    				</td>
+    			</tr>
+                <tr class="<?= $contact->has_shipto() ? 'hidden' : ''; ?>">
+    				<td class="control-label">Acknowledgement Contact</td>
+    				<td>
+    					<?= array_flip($config->yesnoarray)[$contact->ackcontact]; ?>
+    				</td>
+    			</tr>
+    			<tr>
+    				<td class="control-label">Buying Contact</td>
+    				<td>
+    					<?= $config->buyertypes[$contact->buyingcontact]; ?>
+    				</td>
+    			</tr>
+    			<tr>
+    				<?php if ($config->cptechcustomer == 'stat') : ?>
+    					<td class="control-label">End User</td>
+    				<?php else : ?>
+    					<td class="control-label">Certificate Contact</td>
+    				<?php endif; ?>
+    				
+    				<td>
+    					<?= array_flip($config->yesnoarray)[$contact->certcontact]; ?>
+    				</td>
+    			</tr>
+            </tbody>
+        </table>
     <div class="panel-footer">
         <a href="<?= $editpage->getUrl(); ?>" class="btn btn-success btn-sm"> <i class="fa fa-pencil" aria-hidden="true"></i> Edit Contact</a>
     </div>
