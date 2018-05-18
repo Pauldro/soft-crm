9e981f0b389a1669646bb52133c1c7ac2a4819b5
bbullemer <barbara.bullemer@gmail.com>
2018-05-18 09:44:40 -0500

    Quote Line Item Detail
    - added information from line detail to item detail modal
    - fixed headers for quote details screen

--- site/templates/content/view/details/view-quote-details.php
+++ site/templates/content/view/details/view-quote-details.php
@@ -3,10 +3,14 @@
 		<h3>Item Details</h3>
 		<table class="table table-bordered table-condensed">
 			<tr> <td>Item ID</td> <td><?= $linedetail['itemid']; ?></td> </tr>
+			<tr> <td>Warehouse</td> <td><?= $linedetail['whse']; ?></td> </tr>
 			<tr> <td>Unit of Measurement</td> <td><?= $linedetail['uom']; ?></td> </tr>
 			<tr> <td>Requested Ship Date</td> <td><?= $linedetail['rshipdate']; ?></td> </tr>
 			<tr> <td>Kit Item</td> <td><?= $linedetail['kititemflag']; ?></td> </tr>
 			<tr> <td>Special Order</td> <td><?= $linedetail['spcord']; ?></td> </tr>
+			<tr> <td>Quantity</td> <td><?= number_format($linedetail['ordrqty'], 0); ?></td> </tr>
+			<tr> <td>Price</td> <td>$ <?= number_format($linedetail['ordrprice'], 2); ?></td> </tr>
+			<tr> <td>Total</td> <td>$ <?= $linedetail['ordrtotalprice']; ?></td> </tr>
 			<tr> <td>Tax</td> <td><?= $linedetail['taxcode']; ?></td> </tr>
 		</table>
 	</div>
