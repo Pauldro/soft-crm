7a188a57ededf6402bc8de55c91185737dad35f5
bbullemer <barbara.bullemer@gmail.com>
2018-05-29 13:07:04 -0500

    Edit details
    - commented out Add Item button

--- site/templates/content/edit/orders/order-details/details-page.php
+++ site/templates/content/edit/orders/order-details/details-page.php
@@ -1,10 +1,10 @@
 <div id="sales-order-details ">
 	<div class="form-group"><?php include $config->paths->content.'edit/orders/order-details/order-details.php'; ?></div>
-	<div class="text-center form-group">
+	<!-- <div class="text-center form-group">
 		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#item-lookup-modal">
 		    <span class="glyphicon glyphicon-plus"></span> Add Item
 		</button>
-	</div>
+	</div> -->
 	<div class="row">
 		<div class="col-xs-3 col-sm-7"></div>
 	    <div class="col-xs-9 col-sm-5">
