71b1fd75456d4b0cf64e5ca634cfb141a25adaad
bbullemer <barbara.bullemer@gmail.com>
2018-07-02 11:34:40 -0500

    Print Preview for Sales Order
    - changed property names for classes so they would be displayed

--- site/templates/content/print/orders/outline.php
+++ site/templates/content/print/orders/outline.php
@@ -9,7 +9,7 @@
 		<img src="<?= $appconfig->companylogo->url; ?>" alt="<?= $appconfig->companydisplayname.' logo'; ?>" style="max-width: 100%;">
 	</div>
 	<div class="col-xs-7 text-right">
-		<h1>Order # <?= $ordn; ?></h1>
+		<h1>Order # <?= $order->orderno; ?></h1>
 		</br>
 	</div>
 </div>
@@ -21,19 +21,19 @@
 			<a href="<?= $config->documentstorage.$pdfmaker->filename; ?>" target="_blank" class="btn btn-primary hidden-print"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> View PDF</a>
 		<?php endif; ?>
 	</div>
-	
+
 	<div class="col-xs-6">
 		<div class="row">
 			<table class="pull-right">
-				<tr> <td class="col-xs-6"><label>Order Date:</label></td> <td class="col-xs-6 text-right"><?= $order->orderdate; ?></td> </tr>
-				<tr> <td class="col-xs-6"><label>Request Date:</label></td> <td class="col-xs-6 text-right"><?= $order->rqstdate; ?></td> </tr>
+				<tr> <td class="col-xs-6"><label>Order Date:</label></td> <td class="col-xs-6 text-right"><?= DplusDateTime::format_date($order->orderdate); ?></td> </tr>
+				<tr> <td class="col-xs-6"><label>Request Date:</label></td> <td class="col-xs-6 text-right"><?= DplusDateTime::format_date($order->request_date); ?></td> </tr>
 				<tr> <td class="col-xs-6"><label>Status:</label></td> <td class="col-xs-6 text-right"><?= $order->status; ?></td> </tr>
 				<tr> <td class="col-xs-6"><label>Customer ID:</label></td> <td class="col-xs-6 text-right"><?= $order->custid; ?></td> </tr>
 				<tr> <td class="col-xs-6"><label>Customer PO:</label></td> <td class="col-xs-6 text-right"><?= $order->custpo; ?></td> </tr>
 				<tr> <td class="col-xs-6"><label>Shipping Method:</label></td> <td class="col-xs-6 text-right"><?= $order->shipviadesc; ?></td></tr>
 				<tr> <td class="col-xs-6"><label>Payment Terms:</label></td> <td class="col-xs-6 text-right"><?= $order->termcodedesc; ?></td></tr>
-				<tr> <td class="col-xs-6"><label>Salesperson:</label></td> <td class="col-xs-6 text-right"><?= $order->sp1name; ?></td></tr>
-				<tr> <td class="col-xs-6"><label>Salesperson Email:</label></td> <td class="col-xs-6 text-right"><?= $salespersonjson['data'][$order->sp1]['spemail']; ?></td></tr>
+				<tr> <td class="col-xs-6"><label>Salesperson:</label></td> <td class="col-xs-6 text-right"><?= $order->salesperson_1; ?></td></tr>
+				<tr> <td class="col-xs-6"><label>Salesperson Email:</label></td> <td class="col-xs-6 text-right"><?= $salespersonjson['data'][$order->salesperson_1]['spemail']; ?></td></tr>
 			</table>
 		</div>
 	</div>
@@ -43,32 +43,33 @@
 <div class="row">
 	<div class="col-xs-4">
 		<div class="address-header"><h3>Bill-to</h3></div>
+		<?php $customer = Customer::load($order->custid); ?>
 		<address>
-			<?= $order->custname; ?><br>
-			<?= $order->billaddress; ?><br>
-			<?php if (strlen($order->billaddress2) > 0) : ?>
-				<?= $order->billaddress2; ?><br>
+			<?= $customer->name; ?><br>
+			<?= $customer->addr1; ?><br>
+			<?php if (strlen($customer->addr2) > 0) : ?>
+				<?= $customer->addr2; ?><br>
 			<?php endif; ?>
-			<?= $order->billcity.", ".$order->billstate." ".$order->billzip; ?>
+			<?= $customer->city.", ".$customer->state." ".$customer->zip; ?>
 		</address>
 	</div>
 	<div class="col-xs-4">
 		<div class="address-header"><h3>Ship-to</h3></div>
 		<address>
-			<?= $order->shipname; ?><br>
-			<?= $order->shipaddress; ?><br>
-			<?php if (strlen($order->shipaddress2) > 0) : ?>
-				<?= $order->shipaddress2; ?><br>
+			<?= $order->shipto_name; ?><br>
+			<?= $order->shipto_address1; ?><br>
+			<?php if (strlen($order->shipto_address2) > 0) : ?>
+				<?= $order->shipto_address2; ?><br>
 			<?php endif; ?>
-			<?= $order->shipcity.", ".$order->shipstate." ".$order->shipzip; ?>
+			<?= $order->shipto_city.", ".$order->shipto_state." ".$order->shipto_zip; ?>
 		</address>
 	</div>
 	<div class="col-xs-4">
 		<div class="address-header"><h3>Contact</h3></div>
 		<address>
 			<?= $order->contact; ?><br>
-			<?= $order->phone; ?><br>
-			<?= $order->email; ?>
+			<?= $page->stringerbell->format_phone($order->phone); ?><br>
+			<?= $order->contact_email; ?>
 		</address>
 	</div>
 </div>
@@ -81,7 +82,7 @@
 		<th class="text-right" width="100">Price</th>
 		<th class="text-right">Line Total</th>
 	</tr>
-	<?php  $details = $orderdisplay->get_orderdetails($order); ?>
+	<?php $details = $orderdisplay->get_orderdetails($order); ?>
 	<?php foreach ($details as $detail) : ?>
 		<tr class="detail table-bordered">
 			<td>
@@ -102,26 +103,25 @@
 	<div class="col-xs-3">
 		<table class="table table-condensed pull-right">
 			<tr>
-				<td class="col-xs-6"><label>Subtotal</label></td> 
-				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->subtotal); ?></td>
+				<td class="col-xs-6"><label>Subtotal</label></td>
+				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->subtotal_nontax); ?></td>
 			</tr>
 			<tr>
-				<td class="col-xs-6"><label>Tax</label></td> 
-				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->salestax); ?></td>
+				<td class="col-xs-6"><label>Tax</label></td>
+				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->total_tax); ?></td>
 			</tr>
 			<tr>
-				<td class="col-xs-6"><label>Freight</label></td> 
-				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->freight); ?></td>
+				<td class="col-xs-6"><label>Freight</label></td>
+				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->total_freight); ?></td>
 			</tr>
 			<tr>
-				<td class="col-xs-6"><label>Misc.</label></td> 
-				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->misccost); ?></td>
+				<td class="col-xs-6"><label>Misc.</label></td>
+				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->total_misc); ?></td>
 			</tr>
 			<tr class="active">
-				<td class="col-xs-6"><label>Total</label></td> 
-				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->ordertotal); ?></td>
+				<td class="col-xs-6"><label>Total</label></td>
+				<td class="text-right col-xs-6">$ <?= $page->stringerbell->format_money($order->total_order); ?></td>
 			</tr>
 		</table>
 	</div>
 </div>
-	
