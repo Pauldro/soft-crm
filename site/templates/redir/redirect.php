ae333783fdd99616713e13842351c81dcdb9f527
Paul Gomez <pauldro952@outlook.com>
2018-09-13 09:17:45 -0500

    User role redirect after Login

--- site/templates/redir/redirect.php
+++ site/templates/redir/redirect.php
@@ -8,12 +8,12 @@
 		$session->remove('loggingin');
 
 		if (!$user->loggedin) {
-			$L = $config->pages->login;
+			$url = $config->pages->login;
 		} else {
-			if (in_array($user->mainrole, array_keys($config->rolehomepages))) {
-				$L = $config->user_roles[$user->mainrole]['homepage']; 
+			if (in_array($user->mainrole, array_keys($config->user_roles))) {
+				$url = $config->user_roles[$user->mainrole]['homepage']; 
 			} else {
-				$L = $config->user_roles['default']['homepage']; 
+				$url = $config->user_roles['default']['homepage']; 
 			}
 		}
 	}
