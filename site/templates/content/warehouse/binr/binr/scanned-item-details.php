3b0935be60edf1d3f0890aa4f452cb61ae78e1a7
Paul Gomez <pauldro952@outlook.com>
2018-11-29 08:58:58 -0600

    Binr Scanned item details is now a table instead of div based

--- site/templates/content/warehouse/binr/binr/scanned-item-details.php
+++ site/templates/content/warehouse/binr/binr/scanned-item-details.php
@@ -1,10 +1,28 @@
-<div class="row">
-    <div class="col-sm-3">
-        <strong>ItemID:</strong>
-    </div>
-    <div class="col-sm-9">
-        <?= $item->itemid; ?> <br>
-        <?= $item->desc1;  ?> <br>
-        <?= $item->desc2;  ?>
-    </div>
-</div>
+<table class="table table-condensed table-striped">
+	<tr>
+		<td>Item</td>
+		<td><?= $item->get_itemidentifier(); ?></td>
+	</tr>
+	<tr>
+		<td>Item ID</td>
+		<td> <?= $item->itemid; ?></td>
+	</tr>
+	<tr>
+		<td>Description</td>
+		<td><?= $item->desc1; ?></td>
+	</tr>
+	<?php if (!empty($item->desc2)) : ?>
+		<tr>
+			<td></td>
+			<td><?= $item->desc2; ?></td>
+		</tr>
+	<?php endif; ?>
+	<tr>
+		<td>Origin</td>
+		<td><?= strtoupper($item->xorigin); ?></td>
+	</tr>
+	<tr>
+		<td>X-ref Item ID</td>
+		<td><?= $item->xitemid; ?></td>
+	</tr>
+</table>
