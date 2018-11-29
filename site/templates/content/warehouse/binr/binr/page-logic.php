24155b95e71d11f6366adecf47ecbb6a88e5c975
Paul Gomez <pauldro952@outlook.com>
2018-11-29 08:58:18 -0600

    updated binr page-logic

--- site/templates/content/warehouse/binr/binr/page-logic.php
+++ site/templates/content/warehouse/binr/binr/page-logic.php
@@ -2,6 +2,8 @@
 	// NOTE THE FOLLOWING IS FOR NORMAL ITEMS ONLY
 	
 	if ($input->get->scan) {
+		$page->fullURL->query->remove('scan');
+		
 		$scan = $input->get->text('scan');
 		$resultscount = count_invsearch(session_id());
 		
@@ -11,7 +13,23 @@
 			$item = get_firstinvsearchitem(session_id());
 			$page->body = $config->paths->content."{$page->path}binr-form.php";
 		} else {
-			
+			$items = get_invsearchitems(session_id());
+			$page->body = $config->paths->content."{$page->path}inventory-results.php";
+		}
+	} elseif ($input->get->serialnbr) {
+		
+	} elseif ($input->get->lotnbr) {
+		
+	} elseif ($input->get->itemid) {
+		$itemID = $input->get->text('itemid');
+		$resultscount = count_invsearchbyitemid(session_id(), $itemID);
+		
+		if ($resultscount == 1) {
+			$item = get_firstinvsearchitem(session_id());
+			$page->body = $config->paths->content."{$page->path}binr-form.php";
+		} else {
+			$items = get_invsearchitems(session_id());
+			$page->body = $config->paths->content."{$page->path}inventory-results.php";
 		}
 	} else {
 		$page->body = $config->paths->content."{$page->path}item-form.php";
