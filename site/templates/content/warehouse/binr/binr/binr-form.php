85a775355967f8403c2d1895815d237eb151dce5
Paul Gomez <pauldro952@outlook.com>
2018-11-29 08:58:26 -0600

    Update binr-form.php

--- site/templates/content/warehouse/binr/binr/binr-form.php
+++ site/templates/content/warehouse/binr/binr/binr-form.php
@@ -5,11 +5,11 @@
 		<div>
 			<h3>From</h3>
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-sm-6 form-group">
 					<label for="from-bin">Bin</label>
 					<input type="text" class="form-control input-sm" name="from-bin">
 				</div>
-				<div class="col-sm-6">
+				<div class="col-sm-6 form-group">
 					<label for="bin-qty">Qty</label>
 					<div class="input-group">
 						<input type="text" class="form-control input-sm text-right" name="bin-qty" disabled>
@@ -25,11 +25,11 @@
 		<div>
 			<h3>To</h3>
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-sm-6 form-group">
 					<label for="to-bin">Bin</label>
 					<input type="text" class="form-control input-sm" name="to-bin">
 				</div>
-				<div class="col-sm-6">
+				<div class="col-sm-6 form-group">
 					<label for="move-qty">Qty</label>
 					<input type="text" class="form-control input-sm text-right" name="move-qty">
 				</div>
