3c6114918080c4aab61ccd66a21a87d13f980c28
Paul Gomez <pauldro952@outlook.com>
2018-12-17 15:13:58 -0600

    Validation
    Validate Item has qty
    
    Validate bin IDs, separate, validation functions for binr submission

--- site/templates/content/warehouse/binr/binr/inventory-results.php
+++ site/templates/content/warehouse/binr/binr/inventory-results.php
@@ -6,7 +6,7 @@
 <div class="list-group">
 	<?php if ($resultscount) : ?>
 		<?php foreach ($items as $item) : ?>
-			<a href="<?= BinrSession::get_binritembinsURL($item); ?>" class="list-group-item">
+			<a href="<?= BinrSession::get_binritembinsURL($item); ?>" class="list-group-item binr-inventory-result" data-desc="<?= $item->get_itemtypepropertydesc(); ?>" data-item="<?= $item->get_itemidentifier(); ?>" data-qty="<?= $item->qty; ?>">
 				<div class="row">
 					<div class="col-xs-12">
 						<h4 class="list-group-item-heading"><?= strtoupper($item->get_itemtypepropertydesc()) . " " . $item->get_itemidentifier(); ?></h4>
@@ -22,11 +22,8 @@
 	<?php else : ?>
 		<a href="#" class="list-group-item">
 			<div class="row">
-				<div class="col-xs-2">
-					<h4 class="list-group-item-heading"></h4>
-				</div>
-				<div class="col-xs-10">
-					<h4 class="list-group-item-heading">No Items Match your query.</h4>
+				<div class="col-xs-12">
+					<h3 class="list-group-item-heading">No items found for "<?= $input->get->text('scan'); ?>"</h3>
 					<p class="list-group-item-text"></p>
 				</div>
 			</div>
