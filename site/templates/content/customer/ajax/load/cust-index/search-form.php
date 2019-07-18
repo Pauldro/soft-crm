9c22b57201f22cbf7b3429d060e5096db7f73aaa
bbullemer <barbara.bullemer@gmail.com>
2019-07-18 10:16:01 -0500

    Update search-form.php
    - quick entry cust search table was getting smaller with each page change. Multiple divs with the same ID, so had to change

--- site/templates/content/customer/ajax/load/cust-index/search-form.php
+++ site/templates/content/customer/ajax/load/cust-index/search-form.php
@@ -6,7 +6,7 @@
 	$states = $custindex->get_statesbylogin();
 ?>
 <?php echo $url->url; ?>
-<div id="cust-index">
+<div id="cust-search-index">
 	<form action="<?= $config->pages->ajax."load/customers/cust-index/"; ?>" method="POST" id="cust-index-search-form" class="allow-enterkey-submit">
 	    <div class="form-group">
 	        <?php if ($input->get->function) : ?>
